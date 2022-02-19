b81038d734c8b0df2d6fa16749de54d7bf87f687
George Pastushok <gpastushok@yahoo.com>
2022-02-19 00:16:40 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// take input from the user in the command line
+//
+// ask for project title
+//
+// ask for description
+//
+// installation instructions
+//
+// usage information
+//
+// contribution guidelines
+//
+// test instructions
+//
+// prompt the user to select a license from a list
+//
+// ask for a github username
+//
+// ask for an email address
+//
+// create a table of contents that has clickable links to the parts of the readme
